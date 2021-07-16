103e4674869c5e7178de836beaf070e14d69d399
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-07-16 23:15:24 +0530

    Adding CORS error removal

--- server.js
+++ server.js
@@ -4,4 +4,15 @@ import mongoose from 'mongoose';
 import Routes from './routes/routes'
 import Error from './app/Exceptions/error';
 require('dotenv').config();
+const app = express();
+app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+}); 
