83558e96c83ad9b10a7116257f3b4c5a87201266
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-07-16 23:40:15 +0530

    Adding user profile data and user group data

--- routes/accountApi.js
+++ routes/accountApi.js
@@ -13,4 +13,15 @@ AccountApiRouter.post('/account/login/', (request, response) => {
   const accountController = new AccountController(response);
   accountController.loginAccount(request);
 });
+
+AccountApiRouter.post('/account/:uid', (request, response) => {
+  const accountController = new AccountController(response);
+  accountController.getData(request);
+});
+
+AccountApiRouter.post('/account/data/:uid', (request, response) => {
+  const accountController = new AccountController(response);
+  accountController.verifyUsername({_id:request.params.uid});
+});
+
 export default AccountApiRouter;
