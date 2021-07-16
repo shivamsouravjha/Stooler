02279545376caf0331888086cd3855c833b60acd
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-07-17 00:23:36 +0530

    routes to fetch groups based on user ownership

--- routes/groupApi.js
+++ routes/groupApi.js
@@ -15,6 +15,26 @@ GroupApiRouter.post('/getgroups/:uid', (request, response) => {
 });
 
 
+GroupApiRouter.post('/getjoingroups/:uid', (request, response) => {
+  const groupController = new GroupController(response);
+  groupController.getGroups(request,false);
+});
+
+GroupApiRouter.post('/groupmembers/:groupId', (request, response) => {
+  const groupController = new GroupController(response);
+  groupController.getMembers(request);
+});
+
+GroupApiRouter.post('/transferownership/:context/:uid', (request, response) => {
+  const groupController = new GroupController(response);
+  groupController.getOwner(request);
+});
+
+GroupApiRouter.post('/:context/:uid/', (request, response) => {
+  const groupController = new GroupController(response);
+  groupController.userGroup(request);
+});
+
 GroupApiRouter.post('/:groupId', (request, response) => {
   const groupController = new GroupController(response);
   groupController.getGroup(request);
