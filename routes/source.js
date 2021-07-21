e1b19a1bd810668af312fbfd7a74d4c7b1e2c4ab
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-07-22 01:05:06 +0530

    documenting source

--- routes/source.js
+++ routes/source.js
@@ -3,31 +3,37 @@ import SourceController from '../app/Controllers/sourceController';
 
 const SourceApiRouter = express.Router();
 
+// Adding source to group and permting for userid
 SourceApiRouter.post('/add/:gid/:uid', (request, response) => {
   const groupController = new SourceController(response);
   groupController.addSource(request);
 });
 
+//getting sources for a group (gid = group id)
 SourceApiRouter.post('/getcompany/:gid', (request, response) => {
   const groupController = new SourceController(response);
   groupController.getSources(request);
 });
 
+//getting source details for a source
 SourceApiRouter.post('/getcompanydetails/:sid', (request, response) => {
   const groupController = new SourceController(response);
   groupController.getSource(request);
 });
 
+// edit or add source by user
 SourceApiRouter.post('/:edit/sources/:sid/:uid', (request, response) => {
   const groupController = new SourceController(response);
   groupController.editSource(request);
 });
 
+//getting all the sources that need user approval
 SourceApiRouter.post('/approve/:uid/', (request, response) => {
   const groupController = new SourceController(response);
   groupController.getAprroval(request);
 });
 
+//approving the source(sid) by user (uid)
 SourceApiRouter.post('/setapproval/:sid/:uid/', (request, response) => {
   const groupController = new SourceController(response);
   groupController.setApproval(request);
