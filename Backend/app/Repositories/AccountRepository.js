a5458036a6bfccef79b0564357c8ffbd3b01b428
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-07-23 15:47:46 +0530

    Token added in signup

--- Backend/app/Repositories/AccountRepository.js
+++ Backend/app/Repositories/AccountRepository.js
@@ -1,4 +1,5 @@
 import UserModel from "../Models/userModel";
+import jwt from 'jsonwebtoken';
 
 export default class AccountRepository {
     async findUsername(obj){
@@ -8,17 +9,19 @@ export default class AccountRepository {
         } catch (error) {
             return "error at finding"
         }
-        return 
-    }
+}
     async addUser(obj){
-            const {name,panNumber,aadhar,username,email,password,number}=obj
-            const userModel = new UserModel({name,panNumber,aadhar,username,email,password,number})
-            try{
-                console.log(userModel)
-                await userModel.save();
-            } catch (error) {
-                return "error at adding"
-            }
-            return {"success":true};
+        const {name,panNumber,aadhar,username,email,password,number}=obj
+        const userModel = new UserModel({name,panNumber,aadhar,username,email,password,number})
+        let userDetails;
+        let token;
+        try{
+            userDetails =  await userModel.save();
+            token = jwt.sign({userid:userDetails.id,email:userDetails.email},process.env.secretcode,{expiresIn:'7d'});
+        } catch (error) {
+            return "error at adding"
+        }
+        return {"success":true,"token":token};
     }
+
 }
--- Backend/app/Services/AccountService.js
+++ Backend/app/Services/AccountService.js
@@ -1,6 +1,7 @@
 import at from 'v-at';
 import AccountRepository from '../Repositories/AccountRepository.js';
 import * as Exceptions from '../Exceptions/Exceptions';
+import bycrypt from 'bcryptjs';
 
 export default class AccountService{
     constructor() {
@@ -10,16 +11,28 @@ export default class AccountService{
 
     async addAccount(args) {
         try {
-            let verifyUsername = await this.verifyUsername(args.username,args.panCard,args.email,args.number,args.aadhar)
+            let verifyUsername =  this.verifyUsername(args.username,args.panCard,args.email,args.number,args.aadhar)
             if(verifyUsername.username){
                 throw (new Exceptions.ConflictException("details already exist"));
             }
+            let hasedPassowrd = await this.hidepassword(args.password)
+            args.password = hasedPassowrd
             let accountInfo = await this.repository.addUser(args);
             return accountInfo
         } catch (error) {
         throw error;
         }
     }
+
+    async hidepassword(args) {
+        try{
+            let hashed = await bycrypt.hash(args,12)
+            return hashed
+        } catch (error) {
+            throw error;
+        }
+    }
+
 
     async verifyUsername(args) {
         try {
@@ -29,5 +42,7 @@ export default class AccountService{
         throw error;
         }
     }
+
+
 
 }
