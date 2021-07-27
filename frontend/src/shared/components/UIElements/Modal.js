228abc469d815ca7acb3f333e28a3c0a7ad5a086
Shrestha25 <25shrestha97@gmail.com>
2021-07-27 11:36:35 +0530

    login error solve

--- frontend/src/shared/components/UIElements/Modal.js
+++ frontend/src/shared/components/UIElements/Modal.js
@@ -25,7 +25,7 @@ const ModalOverlay = props => {
       </form>
     </div>
   );
-  return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
+  return ReactDOM.createPortal(content, document.getElementById('root'));
 };
 
 const Modal = props => {
