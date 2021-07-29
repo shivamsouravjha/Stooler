522f2f38c2f4933825741064e42cbf78f79edbce
Shrestha25 <25shrestha97@gmail.com>
2021-07-29 23:44:22 +0530

    improved login and signup form

--- frontend/src/shared/util/validators.js
+++ frontend/src/shared/util/validators.js
@@ -5,6 +5,8 @@ const VALIDATOR_TYPE_MIN = 'MIN';
 const VALIDATOR_TYPE_MAX = 'MAX';
 const VALIDATOR_TYPE_EMAIL = 'EMAIL';
 const VALIDATOR_TYPE_FILE = 'FILE';
+const VALIDATOR_TYPE_FIXLENGTH = 'FIXLENGTH';
+const VALIDATOR_TYPE_PASSWORD = 'PASSWORD';
 
 export const VALIDATOR_REQUIRE = () => ({ type: VALIDATOR_TYPE_REQUIRE });
 export const VALIDATOR_FILE = () => ({ type: VALIDATOR_TYPE_FILE });
@@ -16,6 +18,13 @@ export const VALIDATOR_MAXLENGTH = val => ({
   type: VALIDATOR_TYPE_MAXLENGTH,
   val: val
 });
+export const VALIDATOR_FIXLENGTH = val => ({
+  type: VALIDATOR_TYPE_FIXLENGTH,
+  val: val
+});
+export const VALIDATOR_PASSWORD = () =>({
+  type: VALIDATOR_TYPE_PASSWORD
+});
 export const VALIDATOR_MIN = val => ({ type: VALIDATOR_TYPE_MIN, val: val });
 export const VALIDATOR_MAX = val => ({ type: VALIDATOR_TYPE_MAX, val: val });
 export const VALIDATOR_EMAIL = () => ({ type: VALIDATOR_TYPE_EMAIL });
@@ -23,6 +32,13 @@ export const VALIDATOR_EMAIL = () => ({ type: VALIDATOR_TYPE_EMAIL });
 export const validate = (value, validators) => {
   let isValid = true;
   for (const validator of validators) {
+    if (validator.type === VALIDATOR_TYPE_PASSWORD) {
+      var  Length=value.trim().length >= 8;
+      var  Numbers=value.trim().match(/\d+/g);
+      var  Upper=value.trim().match(/[A-Z]/);
+      var  Lower=value.trim().match(/[a-z]/);
+      isValid = isValid && Length && Lower && Numbers && Upper;
+    }
     if (validator.type === VALIDATOR_TYPE_REQUIRE) {
       isValid = isValid && value.trim().length > 0;
     }
@@ -31,6 +47,9 @@ export const validate = (value, validators) => {
     }
     if (validator.type === VALIDATOR_TYPE_MAXLENGTH) {
       isValid = isValid && value.trim().length <= validator.val;
+    }
+    if (validator.type === VALIDATOR_TYPE_FIXLENGTH) {
+      isValid = isValid && value.trim().length == validator.val;
     }
     if (validator.type === VALIDATOR_TYPE_MIN) {
       isValid = isValid && +value >= validator.val;
