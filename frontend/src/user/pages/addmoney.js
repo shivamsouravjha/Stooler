23cdf49839011c5d93d212c4a8d13e6d42df6834
Shivam Sourav Jha <60891544+shivamsouravjha@users.noreply.github.com>
2021-10-02 16:36:59 +0530

    Revert "add money"

--- frontend/src/user/pages/addmoney.js
+++ frontend/src/user/pages/addmoney.js
@@ -1,77 +1,17 @@
-import React, { useEffect, useState,Component} from 'react';
-import { useParams } from 'react-router-dom';
-import Input from '../../shared/components/FormElements/Input';
-import "./Auth.css";
-import { useHttpClient } from '../../shared/hooks/http-hook';
-import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
-import SuccessModal from '../../shared/components/UIElements/Success';
+import React from 'react';
+import '../../App.css';
+import PieChart from '../components/PieChart';
+import Users from './Users';
+import { Link } from 'react-router-dom';
+import Transaction from './transaction'
 
-const  EditSource = ()=>{
-   const {sendRequest} = useHttpClient();
-   const [isLoading, setIsLoading] = useState(false);
-   const [success, setSuccess] = useState();
-   const [error, setError] = useState();
-    const [amount,setamount]=useState();
-    const sid = useParams().sid;
-    var userId = localStorage.getItem('__react_session__');
-    userId = JSON.parse(userId)
-    userId = userId['userId']
-    const onSubmitform = async e =>{
-        e.preventDefault();
-        try{   
-            setIsLoading(true);
-            var body={"sum":amount};
-            body = JSON.stringify(body)
-            const responseData = await sendRequest(
-                `https://stool-back.herokuapp.com/api/addsum/account/${userId}/`,"POST",body,{
-                    'Content-Type': 'application/json'
-                }
-            );
-            if(responseData['status']!=200 && responseData['status']!=202){
-                throw responseData.error;
-            }
-            setSuccess(responseData.data.message || 'Something went wrong, please try again.');
-            setIsLoading(false);
-            setError(false);
-            // window.location="/";
-        }catch(err){
-            setIsLoading(false);
-            setError(err.message || 'Something went wrong, please try again.');
-        }
-    }
-    const successHandler = () => {
-        setSuccess(null);
-        setError(null);
-      };
+const Profile = () => {
     
-    return (   
-        <React.Fragment>
-        <SuccessModal error={success} onClear={successHandler} />
-            <div className="group_form_div">
-                <center>
-                    <form  action="/" id="event_form"  name="event_form" className="request_form" onSubmit={onSubmitform}>
-                                            {/* form header */}
-                        <h2 className="form_heading">
-                            Add Money to your account
-                        </h2> 
-                        <hr className="investment_hr"/>
-                        <br/>
-                        <label className="labels">
-                         Enter amount<span > * </span> 
-                        </label>
-                        <input type="number" name="amount" className="request_inputs" value={amount} placeholder="New Units" onChange={e =>setamount(e.target.value)} required />
-                        <br/><br/>
-                        <button type="submit" className="join_btns">
-                            Done
-                        </button>
-                    </form> 
-                        
-
-                </center>
-            </div>)
-    </React.Fragment>
-    );
-  
+    return (
+        <div className="App">
+           chall Paisa De
+        </div>
+    )
 };
-
-export default EditSource;
+    
+export default Profile
